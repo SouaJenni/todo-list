b88a4b4f2885f62e8cba1c758af764b6a5682770
SouaJenni <jennifervialli1@gmail.com>
2025-03-24 23:28:38 -0300

    Atualizando as funcionalidades da lista de afazeres e botão de limpar salvando dados no local storage

--- javascript/index.js
+++ javascript/index.js
@@ -3,21 +3,35 @@ const formulario = document.querySelector('form');
 const entradaDeTexto = document.getElementById('afazeres');
 const lista = document.getElementById('listaDeAfazeres');
 
-const adicionarAfazer = (e) => {
+const coisasAFazer = [];
 
+const salvos = JSON.parse(localStorage.getItem('Afazeres')) || [];
+for(const fazer of salvos){
+    adicionarItem(fazer);
+}
+
+const adicionarAfazer = (e) => {
     e.preventDefault();
     const texto = entradaDeTexto.value;
+    adicionarItem(texto);
+    entradaDeTexto.value = '';
+    localStorage.setItem('Afazeres', JSON.stringify(coisasAFazer));
+}
+
+function adicionarItem(texto){
     const item = document.createElement('li');
     item.textContent = texto;
     lista.appendChild(item);
-    entradaDeTexto.value = '';
+    coisasAFazer.push(texto);
 }
 
 const limparLista = () => {
     while(lista.firstChild){
         lista.removeChild(lista.firstChild);
     }
+    localStorage.clear();
 }
 
 botao.addEventListener('click', limparLista);
 formulario.addEventListener('submit', adicionarAfazer);
+
